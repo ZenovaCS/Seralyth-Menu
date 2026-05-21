bc438fe14b4305c4808d84223adb982834fcd787
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2026-05-21 10:50:06 -0500

    Remove GetFileExtension we dont need it at all

--- Utilities/FileUtilities.cs
+++ Utilities/FileUtilities.cs
@@ -62,21 +62,6 @@ namespace Seralyth.Utilities
             return output;
         }
 
-        public static AudioType GetAudioType(string extension)
-        {
-            if (string.IsNullOrEmpty(extension))
-                return AudioType.UNKNOWN;
-
-            return extension.ToLowerInvariant() switch
-            {
-                "mp3" => AudioType.MPEG,
-                "wav" => AudioType.WAV,
-                "ogg" => AudioType.OGGVORBIS,
-                "aiff" => AudioType.AIFF,
-                _ => AudioType.UNKNOWN,
-            };
-        }
-
         public static string GetFullPath(Transform transform)
         {
             if (transform == null)
